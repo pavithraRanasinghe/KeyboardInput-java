281317ff83ad1b89331b2fddcc49c42441ecbd24
PavithraRanasinghe <paviranasinghe20@gamil.com>
2019-02-24 19:22:27 +0530

    first keyboad input

---
+++ KeyboadInput.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class KeyboadInput{
+	public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+		System.out.print("Input first number  : ");
+		int x=input.nextInt();
+		System.out.print("Input second number : ");
+		int y=input.nextInt();
+		System.out.println("First number is  : "+x.);
+		System.out.println("Second number is : "+y);
+	}
+}
